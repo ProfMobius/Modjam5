f082299f43ada6e81d50573d5618f826c2d9d4cb
ProfMobius <profmobius@gmail.com>
2018-04-01 23:59:29 +0200

    First try at rendering a custom picture inside a frame

--- src/main/java/team/thegoldenhoe/cameraobscura/client/renderers/RendererProp.java
+++ src/main/java/team/thegoldenhoe/cameraobscura/client/renderers/RendererProp.java
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraftforge.client.MinecraftForgeClient;
+import team.thegoldenhoe.cameraobscura.common.craftstudio.TilePictureFrame;
 import team.thegoldenhoe.cameraobscura.common.craftstudio.TileProps;
 import team.thegoldenhoe.cameraobscura.utils.ModelHandler;
 
@@ -16,31 +17,34 @@ public class RendererProp extends TileEntitySpecialRenderer<TileProps> {
     private static final Vector3 neghalfBlock = new Vector3(-0.5f, 0, -0.5f);
 
     @Override
-    public void render(TileProps tile, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-        int renderPass = MinecraftForgeClient.getRenderPass();
+    public void render(final TileProps tile, final double x, final double y, final double z, final float partialTicks, final int destroyStage, final float alpha) {
+        final int renderPass = MinecraftForgeClient.getRenderPass();
 
         GlStateManager.pushMatrix();
         GlStateManager.translate(x, y, z); // move rendering to TE rendering Offset XYZ
 
         boolean showOutline = false;
-        RayTraceResult target = Minecraft.getMinecraft().objectMouseOver;
+        final RayTraceResult target = Minecraft.getMinecraft().objectMouseOver;
         if (CSLibMod.displayOutline && tile != null && target != null && (target.typeOfHit == RayTraceResult.Type.BLOCK && target.getBlockPos().equals(tile.getPos()))) {
             showOutline = true;
         }
 
         if (tile != null) {
-            CSClientModelWrapperVBO wrapper = (CSClientModelWrapperVBO) ModelHandler.getModelByID(tile.getRenderingType()).wrapper;
+            final CSClientModelWrapperVBO wrapper = (CSClientModelWrapperVBO) ModelHandler.getModelByID(tile.getRenderingType()).wrapper;
             wrapper.render(tile, partialTicks, renderPass, showOutline, (tile.rotation * 22.5F), Vector3.invY, halfBlock, neghalfBlock);
         }
 
         GlStateManager.popMatrix();
 
-        String greenScreen = tile.tileParams.get("greenScreen");
-        if (tile != null && greenScreen != null) {
+        final String greenScreen = tile.tileParams.get("greenScreen");
+        if (tile instanceof TilePictureFrame && greenScreen != null) {
+            final TilePictureFrame tileFrame = (TilePictureFrame) tile;
+            tileFrame.setPicture("2018-04-01_23.29.21.png");
+
             GlStateManager.pushMatrix();
             GlStateManager.translate(x, y, z); // move rendering to TE rendering Offset XYZ
-            CSClientModelWrapperVBO wrapper = (CSClientModelWrapperVBO) ModelHandler.getModelByID(Integer.valueOf(greenScreen)).wrapper;
-            //wrapper.setGlTextureId(0);
+            final CSClientModelWrapperVBO wrapper = (CSClientModelWrapperVBO) ModelHandler.getModelByID(Integer.valueOf(greenScreen)).wrapper;
+            wrapper.setGlTextureId(tileFrame.getGlTextureID());
             wrapper.render(tile, partialTicks, renderPass, showOutline, (tile.rotation * 22.5F), Vector3.invY, halfBlock, neghalfBlock);
             GlStateManager.popMatrix();
         }
--- src/main/java/team/thegoldenhoe/cameraobscura/common/craftstudio/TilePictureFrame.java
+++ src/main/java/team/thegoldenhoe/cameraobscura/common/craftstudio/TilePictureFrame.java
@@ -1,4 +1,61 @@
 package team.thegoldenhoe.cameraobscura.common.craftstudio;
 
+import com.mia.craftstudio.utils.ImageIOCS;
+import net.minecraft.client.renderer.texture.TextureUtil;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.DimensionManager;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class TilePictureFrame extends TileProps {
+    private String pictureLocation = "";
+    private int glTextureID = 0;
+
+    public void setPicture(final String pictureLocation) {
+        if (pictureLocation == this.pictureLocation) {
+            return;
+        }
+
+        this.pictureLocation = pictureLocation;
+
+        TextureUtil.deleteTexture(glTextureID);
+        glTextureID = 0;
+
+        final String dirName = DimensionManager.getCurrentSaveRootDirectory().getAbsolutePath();
+        final File directory = new File(dirName, "photographs");
+        final File picture = new File(directory, pictureLocation);
+
+        if (!(picture.exists() && picture.isFile())) {
+            return;
+        }
+
+        try {
+            final BufferedImage img = ImageIOCS.read(new FileInputStream(picture));
+            glTextureID = TextureUtil.glGenTextures();
+            TextureUtil.uploadTextureImage(glTextureID, img);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getGlTextureID() {
+        return glTextureID;
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(final NBTTagCompound tagCompound) {
+        super.writeToNBT(tagCompound);
+        tagCompound.setString("pictureLocation", pictureLocation);
+
+        return tagCompound;
+    }
+
+    @Override
+    public void readFromNBT(final NBTTagCompound tagCompound) {
+        super.readFromNBT(tagCompound);
+        setPicture(tagCompound.getString("pictureLocation"));
+    }
 }
