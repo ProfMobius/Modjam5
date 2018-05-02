77a7756399ffa6109628710b78c94042f0041b7f
ProfMobius <profmobius@mobiusstrip.eu>
2018-05-02 23:32:54 +0200

    Fixed camera texture. v1.0.4

--- src/main/java/team/thegoldenhoe/cameraobscura/Info.java
+++ src/main/java/team/thegoldenhoe/cameraobscura/Info.java
@@ -8,7 +8,7 @@ public class Info {
 
 	public static final String NAME = "Camera Obscura";
 
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.4";
 
 	public static final String CLIENT_PROXY = "team.thegoldenhoe.cameraobscura.client.ClientProxy";
 
