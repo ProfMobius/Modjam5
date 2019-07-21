f0ef030be1d52611fcaed883196807a8c9d16be3
Jason Hill <qbfreak@qbfreak.net>
2019-07-21 09:30:13 -0400

    Updated version number

--- src/main/java/team/thegoldenhoe/cameraobscura/Info.java
+++ src/main/java/team/thegoldenhoe/cameraobscura/Info.java
@@ -8,7 +8,7 @@ public class Info {
 
 	public static final String NAME = "Camera Obscura";
 
-	public static final String VERSION = "1.0.4";
+	public static final String VERSION = "1.0.5";
 
 	public static final String CLIENT_PROXY = "team.thegoldenhoe.cameraobscura.client.ClientProxy";
 
